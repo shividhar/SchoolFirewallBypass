29b958191e318f1f7f0c2639109e97a248aaaa00
Shivansh Dhar <shivanshdhar@gmail.com>
2016-07-08 13:30:09 -0400

    Added bypass files

---
+++ script.js
@@ -0,0 +1,4 @@
+var documentTitle = document.title;
+if(documentTitle == "Web Page Blocked"){
+	window.location.replace("http://toxic-panda.appspot.com/" + window.location.host + window.location.pathname);
+}
